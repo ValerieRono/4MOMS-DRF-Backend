e61edb745e467f4e97cbda3473c2562470a07e92
Valerie Rono <valerierono@MacBook-Pro-2.local>
2019-03-15 17:17:35 +0300

    add user views

--- formoms/users/views.py
+++ formoms/users/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from users.models import User
+from users.serializers import UserSerializer
+from rest_framework import generics
 
 # Create your views here.
+class UsersList(generics.ListCreateAPIView):
+    """
+    List all users, or create a new User.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrive, update or delete a user instance
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
