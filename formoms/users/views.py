fbd2ac59502d98d8c3906200e86952715a9bdaca
ValerieRono <ronovalerie@gmail.com>
2019-03-17 09:16:18 +0300

    set up to deploy to heroku

--- formoms/users/views.py
+++ formoms/users/views.py
@@ -1,17 +1,18 @@
 from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+
 from users.models import User
 from users.serializers import UserSerializer
-from rest_framework import generics
 
 # Create your views here.
-class UsersList(generics.ListCreateAPIView):
+class UsersList(ListCreateAPIView):
     """
     List all users, or create a new User.
     """
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+class UserDetail(RetrieveUpdateDestroyAPIView):
     """
     Retrive, update or delete a user instance
     """
