179507a16d0268184b09f6c09949c902b468c18b
ValerieRono <ronovalerie@gmail.com>
2019-03-17 16:20:18 +0300

    implement token based authentication

--- formoms/formoms/urls.py
+++ formoms/formoms/urls.py
@@ -15,12 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
+
 
 from formoms import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.api_root),
-    path('api-auth/', include('rest_framework.urls')),
+    path('api-token-auth/', obtain_jwt_token),
+    path('api-token-refresh/', refresh_jwt_token),
+    path('api-token-verify/', verify_jwt_token),
     path('', include(('users.urls', 'users'), namespace='users'))
 ]
